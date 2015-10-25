7924e6a0fd5eda999e9a60d61daa3f09843447ab
vickytorrance <v.torrance@newcastle.ac.uk>
2015-10-25 18:57:12 +0000

    added functions to design primers and look for ORFs

---
+++ ORFs.py
@@ -0,0 +1,34 @@
+# Vicky Torrance
+#
+# Library: VickyModules
+# Module: ORFs
+#
+# Description: Module for ORF-related scripts
+
+import re
+
+def find_ATG(sequence):
+    ATG = [ATG.append(m.start()) for m in re.finditer('ATG', sequence)]
+    return ATG
+    
+def find_STOP(sequence):
+    STOP=[]
+    for m in re.finditer('TGA', sequence):
+        STOP.append(m.start())
+    for m in re.finditer('TAG', sequence):
+        STOP.append(m.start())   
+    for m in re.finditer('TAA', sequence):
+        STOP.append(m.start())
+    STOP.sort(key=int)    
+    return STOP             
+
+def find_orf(seq):
+    starts=find_ATG(seq)
+    stops=find_STOP(seq)
+    orfs=[]
+    for x in starts:
+        for y in stops:
+            if y>x and (y-x)%3==0 and x>=4:
+                orfs.append(x)
+                break
+    return orfs 
---
+++ Primers.py
@@ -0,0 +1,75 @@
+# Vicky Torrance
+#
+# Library: VickyModules
+# Module: Primers
+#
+# Description: Module for primer-related scripts
+
+def closest(mylist, Number):
+    ''' returns the index of the integer in mylist which is closest to Number '''
+    aux = [abs(Number-num) for num in mylist]
+    return aux.index(min(aux))
+
+def calculateTm(g):
+
+    '''calculates TM value from input string.'''
+
+    numA= g.count('A')
+    numT= g.count('T')
+    numC= g.count('C')
+    numG= g.count('G')
+
+    numAT= numA+ numT
+    numGC= numG+ numC
+
+    tm = 4*numGC + 2*numAT
+
+    return tm
+
+def reverseComp(seq):
+
+    '''Function to calculate reverse complement.'''
+
+    seq2 = seq[::-1]
+    seq3 = seq2.replace('A','t')
+    seq4 = seq3.replace('T', 'A')
+    seq5 = seq4.replace('G', 'c')
+    seq6 = seq5.replace('C', 'G')
+    seq7 = seq6.replace('t', 'T')
+    seq8 = seq7.replace('c', 'C')
+
+    return seq8
+
+
+def designPrimerpair(seq, TM, nmin=15, nmax=30):
+
+    '''returns forward and reverse primers. takes arguments, seq and tm'''
+
+    seqRC= reverseComp(seq)
+    xf = [seq[:a] for a in xrange(nmin, nmax)]
+    xr = [seqRC[:a] for a in xrange(nmin, nmax)]
+
+    xTMs = [calculateTm(x) for x in xf]
+    xrTMs = [calculateTm(x) for x in xr]
+
+    closestT_f = closest(xTMs, TM)
+    Fprimer = xf[closestT_f]
+    tmF = xTMs[closestT_f]
+    closestT = closest(xrTMs, tmF)
+    Rprimer = xr[closestT]
+    tmR = xrTMs[closestT]
+
+    return Fprimer, Rprimer
+
+
+def designPrimerR(seq, TM, dist, nmin=15, nmax=30):
+    ''' Returns a reverse primer exactly dist nucleotides away from start'''
+    new = seq[:dist]
+    xf = [new[-a:] for a in xrange(nmin, nmax)]
+    xTMs = [calculateTm(x) for x in xf]
+    closestT_f = closest(xTMs, TM)
+    Fprimer = reverseComp(xf[closestT_f])
+    tmF = xTMs[closestT_f]
+
+    return Fprimer
+
