134692e37518af29a3b3173927bfba7bb0356ca7
vickytorrance <v.torrance@newcastle.ac.uk>
2015-11-20 14:22:28 +0000

    check commit

---
+++ parse.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import os
+
+imOutDir= "/Users/victoriatorrance/Documents/CANS/data/Output_Data" #"../data/Output_Data"
+exptDesc= open("/Users/victoriatorrance/Documents/CANS/data/Auxiliary/ExptDescription.txt", "r")
+libDesc=  open("/Users/victoriatorrance/Documents/CANS/data/Auxiliary/LibraryDescription.txt", "r") #"../data/Auxiliary/LibraryDescription.txt"
+geneToORF= open("/Users/victoriatorrance/Documents/CANS/data/Auxiliary/ORF2GENE.txt", "r")
+fout=  "/Users/victoriatorrance/Documents/CANS/data)"
+
+
